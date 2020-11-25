ce1e68ad3d69ac7a8dbfa07c940238be333a5ff1
fatfatball <benny1021241130@gmail.com>
2020-11-25 11:38:00 +0800

    Reset master to this commit

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?
-	echo 'php';
-?>
+<?php
+
+
+
+
+class henfatme extends github
+{
+    public function fatfat()
+    {
+		echo 'fatfat2';
+
+		echo 'fatfat 功能1';
+		echo 'fatfat 功能2';
+    }
+    public function localfat()
+    {
+		echo 'localfat';
+
+    }
+	public function xia(){
+		echo 'xia2';
+		echo 'xia2';
+		echo 'xia2';
+	}
+
+
+}
